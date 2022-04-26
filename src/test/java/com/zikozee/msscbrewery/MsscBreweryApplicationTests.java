6d4bcf989f386f5adaad75855385b3a3c8ceb714
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2022-04-27 00:23:07 +0100

    add first commit

---
+++ src/test/java/com/zikozee/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zikozee.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
